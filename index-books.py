580227893eed3663921e9ef29f8fbaab1852d7e9
Philip Woods <philipwoods93@gmail.com>
2022-02-28 13:36:03 -0800

    Finished parsing logic

--- index-books.py
+++ index-books.py
@@ -9,13 +9,37 @@ from collections import defaultdict
 Take in top level books directory (and past file to update?)
 Create a sheet for each subdirectory, ignore files in top level dir
 Each sheet has header: Author Title Filetype
-Parse filenames
-    os.path.splitext()
-    Split name on ' -- '
-    If length = 1: Title
-    If length = 2: Author Title (or Title Author if textbook)
-    If length = 3: Author Series Title
 """
+def parse_filename(filename):
+    """
+    Arguments
+    ---------
+    filename: str
+        A string containing the base name of the file
+        Format: [Author -- ][Series -- ]Title.ext
+        Title must be present. Author may be present.
+        If Series is present, Author must also be present.
+
+    Returns
+    -------
+    tuple
+        Contains author, series, title, file type
+    """
+    # Get the extension and the sections of the filename
+    name, ext = os.path.splitext(f)
+    ext = ext[1:].upper() # We don't want the .
+    fields = name.split(' -- ')
+    # Parse the filename
+    # If length = 1: Title
+    # If length = 2: Author Title
+    # If length = 3: Author Series Title
+    if len(fields) == 1:
+        author, series, title = "", "", fields[0]
+    elif len(fields) == 2:
+        author, series, title = fields[0], "", fields[1]
+    elif len(fields) == 3:
+        author, series, title = fields
+    return (author, series, title, ext)
 
 def main(args):
     # Get the subdirectories of the top level directory
@@ -27,10 +51,8 @@ def main(args):
         data = defaultdict(list)
         files = os.listdir(d)
         for f in files:
-            # Get file type
-            ext = os.path.splitext(f)[1][1:].upper()
-            fields = os.path.splitext(f)[0].split(' -- ')
-            # Parse file names (TO DO)
+            # Parse file names
+            (author, series, title, ext) = parse_filename(f)
         sheets[os.basename(d)] = pd.DataFrame(data)
     # Write output index to file
     with pd.ExcelWriter(outfile) as f:
