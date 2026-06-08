2fc1ec26e0a37c8c21c785d3cc761026f69e50a8
Philip Woods <philipwoods93@gmail.com>
2026-06-08 15:34:06 -0700

    Update script to recognize pip packages in conda envs

--- enumerate-conda-packages.py
+++ enumerate-conda-packages.py
@@ -6,15 +6,39 @@ import argparse
 import pandas as pd
 import subprocess as sp
 
-# Currently only lists conda-installed packages, not pip-installed packages
-# May need two steps:
-# 1. List env paths with conda info --envs --json
-#    The relevant output will be results_obj['envs'], which is a list of conda prefix paths
-# 2. Iterate through envs and do conda list -p <env-path> --json
-#    To get relevant data, [(package['name'], package['version'], package['channel']) for package in results_obj]
-# Then do final compilation and formatting
+# Is there a decent way to check for manually installed packages like the prot_loc one on ocean?
+# Maybe compare this output to a list of the things in $CONDA_PREFIX/bin?
 
 def main(args):
+    # Get list of env prefixes
+    env_json = sp.run(['conda', 'info', '--envs', '--json'], capture_output=True, text=True).stdout
+    env_prefixes = json.loads(env_json)['envs']
+    output_records = []
+    for prefix in env_prefixes:
+        # Get env name from env prefix
+        if prefix.count('env') == 0:
+            env_name = "base"
+        else:
+            env_name = os.path.basename(prefix)
+        # Get package info
+        pkg_json = sp.run(['conda', 'list', '--json', '-p', prefix], capture_output=True, text=True).stdout
+        pkgs_obj = json.loads(pkg_json)
+        for pkg in pkgs_obj:
+            record = {
+                    'package': pkg['name'],
+                    'version': pkg['version'],
+                    'env': env_name,
+                    'channel': pkg['channel']
+                    }
+            output_records.append(record)
+    # Create DataFrame for output formatting
+    out = pd.DataFrame.from_records(output_records)
+    out.sort_values(by=['package', 'env'], inplace=True)
+    out.to_csv(args.output, sep='\t', index=False)
+    if args.excel:
+        out.to_excel(args.excel, index=False, freeze_panes=(1,0), autofilter=True)
+
+def main_original(args):
     results_json = sp.run(['conda', 'search', '--envs', '--json'], capture_output=True, text=True).stdout
     results_obj = json.loads(results_json)
     output_records = []
